0e04c2548b651f15aca8723da46a7484654a82b0
zmknnn <kotunonna@gmail.com>
2026-03-07 22:18:58 +0200

    add empty function

---
+++ project_template/app/io/input.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def read_from_console():
+    pass
+
+def read_from_file_builtin(path):
+    pass
+
+def read_from_file_pandas(path):
+    pass
+
---
+++ project_template/app/io/output.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def write_to_console(text):
+    pass
+
+
+def write_to_file_builtin(path, text):
+    pass
+
+
+def write_to_file_pandas(path, data):
+    pass
